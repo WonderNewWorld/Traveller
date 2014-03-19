f8ff0c610b57ed5aa710a1baef1eb9898d435376
AkiraX <cw.akira@gmail.com>
2014-03-19 10:07:20 +0800

    new

---
+++ Traveller/Traveller/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Traveller
+//
+//  Created by TY on 14-3-19.
+//  Copyright (c) 2014年 NewWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Traveller/Traveller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Traveller
+//
+//  Created by TY on 14-3-19.
+//  Copyright (c) 2014年 NewWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
