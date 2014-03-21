ce4b2c22a29c4f6308c814167ec9381518cbb59a
AkiraX <cw.akira@gmail.com>
2014-03-21 15:44:54 +0800

    Revert b8fe61b..1f7f501
    This rolls back to commit b8fe61b36c87dd185f43cce2aa69bfb9f626e567.

--- Traveller/Traveller/NearShopListView.m
+++ Traveller/Traveller/NearShopListView.m
@@ -14,45 +14,21 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        //附近商户列表
-        self.listView = [[UITableView alloc] initWithFrame:CGRectMake(0, 30, self.bounds.size.width, self.bounds.size.height-30)];
-        self.listView.delegate = self;
-        self.listView.dataSource = self;
+        self.backgroundColor = [UIColor yellowColor];
+        self.listView = [[UITableView alloc] initWithFrame:self.bounds];
         [self addSubview:self.listView];
         //seg商铺类型选择
-        NSArray *types = @[@"距离",@"类型",@"排序"];
-        self.segShopType = [[UISegmentedControl alloc] initWithItems:types];
-        self.segShopType.tintColor = [UIColor purpleColor];
-        self.segShopType.frame = CGRectMake(0, 0, self.bounds.size.width, 30);
-        [self addSubview:self.segShopType];
-        
-        self.arrBusiness = [[NSMutableArray alloc] init];
-        [self.arrBusiness addObjectsFromArray:[DPRequest getBusinessListByCity:@"重庆" andCategory:@"美食" andRadius:1000 andSort:7 andPage:1]];
-        
+        [self requestShopList];
     }
     return self;
 }
 
-#pragma mark -UITableViewDataSource Methods
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return [self.arrBusiness count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-    }
-    // 设置数据
-    BusinessModel *temp = self.arrBusiness[indexPath.row];
-    cell.textLabel.text = temp.name;
-    cell.detailTextLabel.text = temp.address;
+- (void)requestShopList{
+    NSString *strSign = [NSString stringWithFormat:@"appkey=%@&sign=%@&",DPAppKey,DPAppSecret];
+    NSString *strParam = @"category=美食&city=上海&latitude=31.18268013000488&longitude=121.42769622802734&sort=7&limit=20&offset_type=1&out_offset_type=1&platform=2";
+    NSURL *url = [NSURL URLWithString:[[NSString stringWithFormat:@"%@%@?%@%@",DPRequestUrl,DPRequestFindShops,strSign,strParam] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    NSString *strContent = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
     
-    return cell;
 }
-
-
-#pragma mark -UITableViewDelegate Methods
 
 @end
