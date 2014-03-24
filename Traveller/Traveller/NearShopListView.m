87a53d804a772981ac1adae0ea5aa183315feddc
AkiraX <cw.akira@gmail.com>
2014-03-24 13:58:47 +0800

    mj

--- Traveller/Traveller/NearShopListView.m
+++ Traveller/Traveller/NearShopListView.m
@@ -27,7 +27,7 @@
         [self addSubview:self.segShopType];
         
         self.arrBusiness = [[NSMutableArray alloc] init];
-        [self.arrBusiness addObjectsFromArray:[DPRequest getBusinessListByCity:@"重庆" andCategory:@"美食" andRadius:1000 andSort:7 andPage:1]];
+        
         //MJ刷新
         [self.listView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"ShopListCell"];
         self.header = [MJRefreshHeaderView header];
@@ -39,6 +39,65 @@
         self.footer.delegate = self;
     }
     return self;
+}
+
+- (void)getArrBusinessWithPage:(int)page{
+    NSArray *arr = [DPRequest getBusinessListByCity:@"重庆" andCategory:@"美食" andRadius:1000 andSort:7 andPage:page];
+    [self.arrBusiness addObjectsFromArray:arr];
+}
+- (void)doneWithView:(MJRefreshBaseView *)refreshView
+{
+    // 刷新表格
+    [self.listView reloadData];
+    // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+    [refreshView endRefreshing];
+}
+#pragma mark - 刷新控件的代理方法
+#pragma mark 开始进入刷新状态
+- (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView
+{
+    NSLog(@"%@----开始进入刷新状态", refreshView.class);
+    
+    // 1.添加假数据
+    if ([refreshView isKindOfClass:[MJRefreshHeaderView class]]) {
+        //刷新
+        self.arrBusiness = [[NSMutableArray alloc] init];
+        self.curPage = 1;
+        [self getArrBusinessWithPage:self.curPage];
+        
+    } else {
+        //添加
+        self.curPage +=1;
+        [self getArrBusinessWithPage:self.curPage];
+    }
+    // 2.2秒后刷新表格UI
+    [self performSelector:@selector(doneWithView:) withObject:refreshView afterDelay:2.0];
+}
+
+#pragma mark 刷新完毕
+- (void)refreshViewEndRefreshing:(MJRefreshBaseView *)refreshView
+{
+    NSLog(@"%@----刷新完毕", refreshView.class);
+}
+
+#pragma mark 监听刷新状态的改变
+- (void)refreshView:(MJRefreshBaseView *)refreshView stateChange:(MJRefreshState)state
+{
+    switch (state) {
+        case MJRefreshStateNormal:
+            NSLog(@"%@----切换到：普通状态", refreshView.class);
+            break;
+            
+        case MJRefreshStatePulling:
+            NSLog(@"%@----切换到：松开即可刷新的状态", refreshView.class);
+            break;
+            
+        case MJRefreshStateRefreshing:
+            NSLog(@"%@----切换到：正在刷新状态", refreshView.class);
+            break;
+        default:
+            break;
+    }
 }
 
 #pragma mark -UITableViewDataSource Methods
